9c29cacad0ba3ba8a63aaf6bd66339e013d1052f
steph <teststephanezaffran@gmail.com>
2017-12-06 14:34:12 +0200

    no message

---
+++ favoritebook/FavoriteBook/Controller/BookFormTableVC.swift
@@ -0,0 +1,58 @@
+//
+//  BookFormTableVC.swift
+//  FavoriteBooks
+//
+//  Created by steph on 05/12/2017.
+//
+
+import UIKit
+
+class 	BookFormTableVC: UITableViewController {
+
+    
+    struct PropertyKeys {
+        static let unwind = "UnwindToBookTable"
+    }
+    
+    var book: Book?
+    
+    
+    @IBOutlet weak var GoButton: UIButton!
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var authorTextField: UITextField!
+    @IBOutlet weak var genreTextField: UITextField!
+    @IBOutlet weak var lengthTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateView()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    func updateView() {
+        guard let book = book else {return}
+        
+        titleTextField.text = book.title
+        authorTextField.text = book.author
+        genreTextField.text = book.genre
+        lengthTextField.text = book.length
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        guard let title = titleTextField.text,
+            let author = authorTextField.text,
+            let genre = genreTextField.text,
+            let length = lengthTextField.text else {return}
+        
+        book = Book(title: title, author: author, genre: genre, length: length)
+        performSegue(withIdentifier: PropertyKeys.unwind, sender: self)
+    }
+    
+    
+   
+
+}
